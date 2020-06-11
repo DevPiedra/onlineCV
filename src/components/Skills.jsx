c5f5f8b2f568e76e14c83487577f818c926adc80
DrRockStone <depiedra@gmail.com>
2020-06-11 09:51:25 -0600

    H2Styled Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, {keyframes} from 'styled-components';
+import H2Styled from '../styled/H2Styled';
 
 const SkillsContainer = styled.div`
     display: grid;
@@ -50,6 +51,7 @@ const SkillsSpan = styled.span`
 
 const Skills = props => (
     <div className="Skills">
+        <H2Styled name="Skills"/>
         <SkillsContainer>
             {props.data.map((skill, index) => (
                 <div className="Skills-item" key={`Skills-${index}`}>
